8120340075ccd791143684328f2065a7b2006ca1
JuanMosquera <jm1128un@gmail.com>
2016-09-01 20:32:41 -0500

    Con fragment Proyecto

--- Proyecto/app/src/main/java/com/example/jfernandomosquera/myapplication/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/jfernandomosquera/myapplication/MainActivity.java
@@ -2,21 +2,31 @@ package com.example.jfernandomosquera.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
+
+    ImageButton btnParqueadero;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        btnParqueadero = (ImageButton)findViewById(R.id.imageButton);
 
-
-
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        Fragment fragment = new PricipalFragment();
+        ft.add(R.id.container, fragment);
+        ft.commit();
 
 
     }
@@ -36,4 +46,21 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    public void onClickImagen(View v) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        Fragment fragment = new ParkFragment();
+        ft.replace(R.id.container, fragment);
+        ft.commit();
+
+    }
+
+    public void inicio(View view){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        Fragment fragment = new PricipalFragment();
+        ft.replace(R.id.container, fragment);
+        ft.commit();
+    }
 }
---
+++ Proyecto/app/src/main/java/com/example/jfernandomosquera/myapplication/ParkFragment.java
@@ -0,0 +1,39 @@
+package com.example.jfernandomosquera.myapplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link ParkFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ */
+public class ParkFragment extends Fragment {
+
+
+    public ParkFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_park, container, false);
+    }
+
+
+
+
+}
